dc9ed1d14bd393e9f79c8761ec20b29906451c9b
Tilly Woodfield <mwoodfield@scottlogic.com>
2020-06-01 13:16:25 +0100

    Redirect to homepage after login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  currentUser: string;
+
+  constructor(private router: Router) {}
+
   login(credentials) {
-    console.log(credentials);
     // TODO: Login request to backend
+    this.currentUser = credentials.name;
+    this.router.navigate(['/']);
   }
 }
