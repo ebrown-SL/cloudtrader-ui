4b73397365c1a0cee11693ba54790e820c80d6ca
Tilly Woodfield <mwoodfield@scottlogic.com>
2020-06-01 11:10:26 +0100

    Add state to login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: [
-    './login.component.css'
-  ]
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm = new FormGroup({
+    name: new FormControl('')
+  });
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+}
