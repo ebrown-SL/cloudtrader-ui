3a234cb0cb064374813a29818453abcd88a10012
Tilly Woodfield <mwoodfield@scottlogic.com>
2020-06-02 13:27:34 +0100

    Unit tests for LoginComponent

---
+++ src/app/login/login.component.spec.ts
@@ -0,0 +1,67 @@
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { LoginComponent } from './login.component';
+import { AuthService } from '../auth/auth.service';
+
+describe('LoginComponent', () => {
+  let fixture: ComponentFixture<LoginComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        ReactiveFormsModule
+      ],
+      declarations: [
+        LoginComponent
+      ],
+      providers: [
+        AuthService
+      ],
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(LoginComponent);
+    });
+  }));
+
+  it('should create a username input', () => {
+    const usernameControl = fixture.debugElement.query(By.css('#username-control'));
+    expect(usernameControl).toBeTruthy();
+  });
+
+  it('should create a password input', () => {
+    const passwordControl = fixture.debugElement.query(By.css('#password-control'));
+    expect(passwordControl).toBeTruthy();
+  });
+
+  it('should update the form object when the input is typed in', () => {
+    fixture.detectChanges();
+
+    const usernameControl = fixture.debugElement.query(By.css('#username-control')).nativeElement;
+    usernameControl.value = 'admin';
+    usernameControl.dispatchEvent(new Event('input'));
+
+    expect(fixture.componentInstance.loginForm.value.username).toEqual('admin');
+  });
+
+  it('should call AuthService login() method with correct object when submit button is clicked', () => {
+    const loginSpy = spyOn(fixture.debugElement.injector.get(AuthService), 'login');
+
+    fixture.detectChanges();
+
+    const usernameControl = fixture.debugElement.query(By.css('#username-control')).nativeElement;
+    usernameControl.value = 'admin';
+    usernameControl.dispatchEvent(new Event('input'));
+
+    const passwordControl = fixture.debugElement.query(By.css('#password-control')).nativeElement;
+    passwordControl.value = 'password';
+    passwordControl.dispatchEvent(new Event('input'));
+
+    const submitButton = fixture.debugElement.query(By.css('button')).nativeElement;
+    submitButton.click();
+
+    expect(loginSpy).toHaveBeenCalledWith({ username: 'admin', password: 'password' });
+  });
+});
