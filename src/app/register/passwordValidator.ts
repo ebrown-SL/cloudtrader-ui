0a6ef222e08909e94507782ee70192b905fb672f
Eddy Brown <eddybrown@me.com>
2020-08-12 11:37:39 +0100

    Code format

--- src/app/register/passwordValidator.ts
+++ src/app/register/passwordValidator.ts
@@ -1,7 +1,9 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 
-export function passwordValidator(control: AbstractControl): ValidationErrors | null {
+export function passwordValidator(
+  control: AbstractControl
+): ValidationErrors | null {
   const password = control.get('password').value;
-  const confirmPassword = control.get('confirmPassword').value;  
-  return password == confirmPassword ? null : { noMatch: true }; 
+  const confirmPassword = control.get('confirmPassword').value;
+  return password == confirmPassword ? null : { noMatch: true };
 }
