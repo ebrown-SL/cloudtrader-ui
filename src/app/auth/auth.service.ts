14e870c5334fdb0774550f4b332b6c58396c96dd
Adina <adina.mihaelaaa@gmail.com>
2020-09-17 10:01:20 +0100

    replaced 'currentUser' access string with constant

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,11 +6,13 @@ import { tap } from 'rxjs/operators';
 import { User } from '../shared/models/user.model';
 import { Credentials } from '../shared/models/credentials.model';
 
+const CURRENT_USER = 'currentUser';
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+  private currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem(CURRENT_USER)));
   public readonly currentUser = this.currentUserSubject.asObservable();
 
   constructor(
@@ -25,9 +27,9 @@ export class AuthService {
   getUserBalance(): Observable<number> {
     return this.httpClient.get<number>(`/user/current/balance`)
       .pipe(tap((balance: number) => {
-        let user = JSON.parse(localStorage.getItem('currentUser'));
+        let user = JSON.parse(localStorage.getItem(CURRENT_USER));
         user.balance = balance;
-        localStorage.setItem('currentUser', JSON.stringify(user));
+        localStorage.setItem(CURRENT_USER, JSON.stringify(user));
         this.currentUserSubject.next(user);
       }));
   }
@@ -35,7 +37,7 @@ export class AuthService {
   register(credentials: Credentials) {
     return this.httpClient.post(`/authentication/register`, credentials)
       .pipe(tap((user: User) => {
-        localStorage.setItem('currentUser', JSON.stringify(user));
+        localStorage.setItem(CURRENT_USER, JSON.stringify(user));
         this.currentUserSubject.next(user);
       }));
   }
@@ -43,13 +45,13 @@ export class AuthService {
   login(credentials: Credentials) {
     return this.httpClient.post(`/authentication/login`, credentials)
       .pipe(tap((user: User) => {
-        localStorage.setItem('currentUser', JSON.stringify(user));
+        localStorage.setItem(CURRENT_USER, JSON.stringify(user));
         this.currentUserSubject.next(user);
       }));
   }
 
   logout() {
-    localStorage.removeItem('currentUser');
+    localStorage.removeItem(CURRENT_USER);
     this.currentUserSubject.next(null);
     this.router.navigate(['/login']);
   }
